b127c0347e137a9df43c0266355d2e041de123d9
nolan <nolan.s.locke@gmail.com>
2023-12-02 00:58:16 -0700

    dirty hack that I am morally opposed to, but it's late

--- src/main/kotlin/DayOne.kt
+++ src/main/kotlin/DayOne.kt
@@ -11,6 +11,25 @@ object DayOne: BaseDay(1) {
     }
 
     override fun printPartTwo(input: List<String>) {
+        val textDigits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine").mapIndexed { index, it -> it to index + 1 }
+        val reversedTextDigits = textDigits.map { it.first.reversed() to it.second }
+        val numberDigits = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9").mapIndexed { index, it -> it to index + 1 }
+        val digitsMap = mutableMapOf<String, Int>()
+        digitsMap.putAll(textDigits)
+        digitsMap.putAll(numberDigits)
+        val reversedDigitsMap = mutableMapOf<String, Int>()
+        reversedDigitsMap.putAll(reversedTextDigits)
+        reversedDigitsMap.putAll(numberDigits)
 
+        val regex = digitsMap.keys.joinToString(separator = "|") { "($it)" }.toRegex()
+        val reverseRegex = reversedDigitsMap.keys.joinToString(separator = "|") { "($it)" }.toRegex()
+
+        var sum = 0
+        input.forEach { line ->
+            val digitOne = digitsMap[regex.findAll(line).first().value]
+            val digitTwo = reversedDigitsMap[reverseRegex.findAll(line.reversed()).first().value]
+            sum += "$digitOne$digitTwo".toInt()
+        }
+        println(sum)
     }
 }
