dc89fda14f9f35a3c1a8992d5473855703f01165
nolan <nolan.s.locke@gmail.com>
2023-12-03 11:47:56 -0700

    day three part two

--- src/main/kotlin/DayThree.kt
+++ src/main/kotlin/DayThree.kt
@@ -27,6 +27,62 @@ object DayThree: BaseDay<Int>(3) {
     }
 
     override fun partTwo(): Int {
-        return 0
+        // 0 is empty, -1 is gear, anything else is the number it's part of
+        // ..98..923...*... == [0,0,98,98,0,0,923,923,923,0,0,0,-1,0,0,0]
+        fun parseInput(input: List<String>): List<List<Int>> {
+            val lines = mutableListOf<List<Int>>()
+            input.forEach { line ->
+                val row = mutableListOf<Int>()
+                var tmpLine = line
+                while (tmpLine.isNotEmpty()) {
+                    when {
+                        tmpLine.first() == '*' -> {
+                            row.add(-1)
+                            tmpLine = tmpLine.drop(1)
+                        }
+
+                        tmpLine.first().isDigit() -> {
+                            val num = tmpLine.takeWhile { it.isDigit() }
+                            num.forEach { _ -> row.add(num.toInt()) }
+                            tmpLine = tmpLine.drop(num.length)
+                        }
+
+                        else -> {
+                            row.add(0)
+                            tmpLine = tmpLine.drop(1)
+                        }
+                    }
+                }
+                lines.add(row)
+            }
+            return lines
+        }
+
+        val input = parseInput(input)
+
+        var sum = 0
+        for ((index, line) in input.withIndex()) {
+            var tmpLine = line
+            while (tmpLine.isNotEmpty()) {
+                val charIndex = line.size - tmpLine.size
+                if (tmpLine.first() == -1) {
+                    //get surrounding numbers
+                    val allSurroundingNumbers = mutableSetOf<Int>()
+                    input.getRange(index - 1, index + 1).forEach { ln ->
+                        ln.getRange(charIndex - 1, charIndex + 1).forEach {
+                            if (it != 0 && it != -1) {
+                                allSurroundingNumbers.add(it)
+                            }
+                        }
+                    }
+
+                    if (allSurroundingNumbers.size == 2) {
+                        sum += (allSurroundingNumbers.first() * allSurroundingNumbers.last())
+                    }
+                }
+                tmpLine = tmpLine.drop(1)
+            }
+        }
+        return sum
     }
 }
--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,10 +1,12 @@
 import java.io.File
 
-fun List<String>.getRange(start: Int, end: Int): List<String> {
-    val list = mutableListOf<String?>()
+fun <T> List<T>.getRange(start: Int, end: Int): List<T> {
+    val list = mutableListOf<T?>()
     (start..end).forEach { list.add(this.getOrNull(it)) }
     return list.filterNotNull()
 }
+
+
 
 abstract class BaseDay<T>(val day: Int) {
     protected open val input by lazy { File(javaClass.getResource("inputs/$day")?.toURI() ?: error("Missing input for day $day")).readLines() }
