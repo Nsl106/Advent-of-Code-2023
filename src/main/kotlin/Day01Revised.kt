e68a728d842c82c82158dca0963b41e845c0b042
nolan <nolan.s.locke@gmail.com>
2023-12-08 02:02:01 -0700

    remove some unnecessary characters from day one

--- src/main/kotlin/Day01Revised.kt
+++ src/main/kotlin/Day01Revised.kt
@@ -1,5 +1,5 @@
 object Day01Revised: BaseDay<Int>(1) {
-    override fun partOne() = input.map { it.filter(Char::isDigit) }.sumOf { "${it.first()}${it.last()}".toInt() }
+    override fun partOne()=input.map{it.filter(Char::isDigit)}.sumOf{(""+it.first()+it.last()).toInt()}
 
     override fun partTwo(): Int {
         val digitsMap = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine").mapIndexed { index, it -> it to index + 1 }
