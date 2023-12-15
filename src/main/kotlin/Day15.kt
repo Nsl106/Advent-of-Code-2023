2903f748d70ad05c4344c0178d14487771d953ee
nolan <nolan.s.locke@gmail.com>
2023-12-14 22:48:03 -0700

    day 15

---
+++ src/main/kotlin/Day15.kt
@@ -0,0 +1,39 @@
+object Day15: BaseDay(15) {
+    private fun String.HASH(): Int {
+        var currentValue = 0
+        forEach { char ->
+            currentValue += char.code
+            currentValue *= 17
+            currentValue %= 256
+        }
+        return currentValue
+    }
+
+    override fun partOne() = input[0].split(",").sumOf { it.HASH() }
+
+    override fun partTwo(): Any {
+        val boxes = MutableList(256) { mutableMapOf<String, Int>() }
+        input[0].split(",").forEach { step ->
+            val label = step.takeWhile(Char::isLetter)
+            val operation = step.first { it in "-=" }
+
+            val boxNumber = label.HASH()
+            when (operation) {
+                '-' -> boxes[boxNumber].remove(label)
+                '=' -> boxes[boxNumber][label] = step.takeLast(1).toInt()
+            }
+        }
+
+        var sum = 0
+        boxes.forEachIndexed { boxID, lenses ->
+            lenses.onEachIndexed { lensIndex, entry ->
+                val boxNumber = boxID + 1
+                val lensNumber = lensIndex + 1
+                val focusNumber = entry.value
+                sum += boxNumber * lensNumber * focusNumber
+            }
+        }
+
+        return sum
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val currentDay = 14
-    val days = listOf(Day01, Day02, Day03, Day04, Day05, Day06, Day07, Day08, Day09, Day10, Day11, Day12, Day13, Day14)
+    val currentDay = 15
+    val days = listOf(Day01, Day02, Day03, Day04, Day05, Day06, Day07, Day08, Day09, Day10, Day11, Day12, Day13, Day14, Day15)
     val revisedDays = listOf(Day01Revised, Day04Revised, Day06Revised)
 
     println("answers for day $currentDay:")
